02b9d7948e577b9c665fff379dfa081be08b5888
毓霖 <1198127872@qq.com>
2019-05-15 14:55:15 +0800

    Revert "风速达到"
    This reverts commit 4aa51d22

--- src/main/java/com/jk/xinx/service/tree/TreeService.java
+++
@@ -1,9 +0,0 @@
-package com.jk.xinx.service.tree;
-
-import com.jk.xinx.pojo.tree.TreeBean;
-
-import java.util.List;
-
-public interface TreeService {
-    List<TreeBean> findWestTreeList(Integer userId);
-}
